f5d55a2b52301a30aff9f284638f7eab6ca182ff
Michel Moraes <michel@nitdesign.com.br>
2020-04-22 21:55:06 +0200

    Adding User Model

--- backend/src/database/migrations/1587583711682-CreateUsers.ts
+++ backend/src/database/migrations/1587583711682-CreateUsers.ts
@@ -1,11 +1,11 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import {MigrationInterface, QueryRunner, Table } from "typeorm";
 
-export class CreateAppointments1587248888937 implements MigrationInterface {
+export default class CreateUsers1587583711682 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -15,12 +15,17 @@ export class CreateAppointments1587248888937 implements MigrationInterface {
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar',
+            isUnique: false,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
           },
           {
             name: 'created_at',
@@ -38,7 +43,6 @@ export class CreateAppointments1587248888937 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
-
 }
